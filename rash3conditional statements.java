deb221dc7374ee3da9594807c1c9c81d16472444
Rashmi-J-K <120172951+Rashmi-J-K@users.noreply.github.com>
2023-03-12 18:17:27 +0530

    Create rash3conditional statements.java

---
+++ rash3conditional statements.java
@@ -0,0 +1,21 @@
+public class rash2 {
+
+
+
+    public static void main(String[] args) {
+        int age=18;
+        if(age==18)
+        {
+            System.out.println("yes you can vote");
+        }
+        else
+        {
+            System.out.println("you can't vote");
+        }
+    }
+
+    }
+
+
+ouput:
+yes you can vote
