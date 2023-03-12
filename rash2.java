4420170248f3bf8078222c59450999cd9a86c9c1
Rashmi-J-K <120172951+Rashmi-J-K@users.noreply.github.com>
2023-03-12 18:06:18 +0530

    Create rash2.java

---
+++ rash2.java
@@ -0,0 +1,20 @@
+public class rash2 {
+
+
+
+    public static void main(String[] args) {
+        int age=15;
+        if(age>=18)
+        {
+            System.out.println("yes you can vote");
+        }
+        else
+        {
+            System.out.println("you can't vote");
+        }
+    }
+
+    }
+
+OUPUT
+you cant vote
