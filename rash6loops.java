c3ec3acfc1b87906ea5591c48e95e5a4a38027cc
Rashmi J K <120172951+Rashmi-J-K@users.noreply.github.com>
2023-03-16 21:16:41 +0530

    Create rash6loops.java

---
+++ rash6loops.java
@@ -0,0 +1,27 @@
+public class loops {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+
+
+
+package rash3;
+
+public class dowhile {
+    public static void main(String[] args) {
+        int b = 10;
+        do {
+            System.out.println(b);
+            b++;
+        }
+        while (b < 5);
+    }
+}
+
+out[ut:10
