c15612084635a58a2a2441291d9657da9c751213
Rashmi J K <120172951+Rashmi-J-K@users.noreply.github.com>
2023-03-22 20:55:57 +0530

    Create rash7-dowhile.java

---
+++ rash7-dowhile.java
@@ -0,0 +1,31 @@
+int i=0;
+        do{
+
+            System.out.println(i);
+            System.out.println("loop starts");
+            if(i==2)
+            {
+                System.out.println("java is great");
+               break;
+            }
+            i++;
+
+
+        }
+        while(i<5);
+        {
+            System.out.println("loop ends");
+        }
+    }
+}
+
+output:
+0
+loop starts
+1
+loop starts
+2
+loop starts
+java is great
+loop ends
+  
