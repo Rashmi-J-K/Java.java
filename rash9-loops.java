449f00ce9c281425dcfb3fa3ab1c8424a103dfd9
Rashmi J K <120172951+Rashmi-J-K@users.noreply.github.com>
2023-04-07 21:41:43 +0530

    Create rash9-loops.java

---
+++ rash9-loops.java
@@ -0,0 +1,17 @@
+package rash3;
+
+public class practicesetonloops {
+    public static void main(String[] args) {
+
+        int sum=0;
+        int n=3;
+        for(int i=0;i<n;i++)
+        {
+            sum=(2*i)+sum;
+        }
+        System.out.println("the sum of even numbers is");
+        System.out.println(sum);
+    }
+}
+output:
+6
